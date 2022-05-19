7b650885d915247b6d798ffb6edd5d63e80d3708
frida cano <105824284+FFCB34@users.noreply.github.com>
2022-05-19 12:14:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.text('Breakfast Menu')
+streamlit.text('OMEGA 3 & blueberyy oatmeal')
+streamlit.text('OMEGA 3 & blueberyy oatmeal')
