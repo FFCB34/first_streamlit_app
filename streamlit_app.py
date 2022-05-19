4e9aac52d3c945959710d48e66fd792a081a12e1
frida cano <105824284+FFCB34@users.noreply.github.com>
2022-05-19 13:05:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
-streamlit.header('Breakfast Menuu')
-streamlit.text('OMEGA 3 & blueberyy oatmeal')
-streamlit.text('Kale, Spnichac & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 OMEGA 3 & blueberyy oatmeal')
+streamlit.text('🥗Kale, Spnichac & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
